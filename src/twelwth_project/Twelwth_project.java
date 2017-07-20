45127b1edb7b37b1cd0246113aa02740a8f84fdb
Boro_s99 <Boro_s99@221-12.od.itstep.local>
2017-07-20 18:33:48 +0300

    Static method

---
+++ src/twelwth_project/Twelwth_project.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package twelwth_project;
+
+class Math 
+    {
+        static int a;
+        int b;
+        
+    static int Temp()
+        {
+            return a = 10;
+        }
+    }
+
+
+public class Twelwth_project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        Math math1 = new Math();
+        Math math2 = new Math();
+        System.out.println(Math.Temp());
+        Math.a = 40;
+        //Math.b = 50;
+        math1.a = 80;
+        math1.b = 60;
+        
+    }
+    
+}
