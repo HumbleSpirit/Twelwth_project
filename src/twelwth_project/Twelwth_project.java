789d69ec52168b133429d98c4bd914b006985432
Boro_s99 <Boro_s99@221-12.od.itstep.local>
2017-07-20 20:14:44 +0300

    Наследование

--- src/twelwth_project/Twelwth_project.java
+++ src/twelwth_project/Twelwth_project.java
@@ -35,17 +35,33 @@ class Math
 
 class Employee
 {
-    private int salary;
+    protected int salary;
     
-    public Employee()
+    public Employee(int a)
     {
-        salary = 100;
+        salary = a;
     }
     public int GetSalary()
     {
         return salary;
     }
 }
+//Наследование определяется ключевым словом extends
+class Manager extends Employee
+{
+    int bonus;
+    Manager(int a, int b)
+    {
+        super(a);
+        //вызов родительского конструктора 
+        bonus = b;
+    }
+    public int GetSalary()
+    {
+        return super.GetSalary() + bonus;
+    }
+}
+
 public class Twelwth_project {
 
     /**
@@ -61,8 +77,17 @@ public class Twelwth_project {
         //math1.a = 80;
         //math1.b = 60;
         //Math.Temp2();
-        Employee empl1 = new Employee();
-        System.out.println();
+        //Employee empl1 = new Employee(5);
+        Manager man1 = new Manager(200, 50);
+        Employee e[] = new Employee[2];
+        //man1.GetSalary();
+        //System.out.println(man1.GetSalary());
+        e[0] = man1;
+        e[1] = new Employee(20);
+        for (int i = 0; i < e.length; i++) 
+        {
+            System.out.println(e[i].GetSalary());    
+        }
     }
     
 }
